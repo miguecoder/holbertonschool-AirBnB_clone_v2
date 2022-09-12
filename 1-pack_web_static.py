47d27db9a7fcf7d0d5a7e75869647b52766cee04
miguecoder <mmarqzm@gmail.com>
2022-09-11 22:41:48 -0500

    script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,12 +12,10 @@ def do_pack():
     """function that generates a .tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions"):
-            file_name = "versions/web_static_{}.tgz".format(date)
-            local("tar -cvzf {} web_static".format(file_name))
-            return file_name
-        else:
+        if isdir("versions") is False:
             local("mkdir versions")
-
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except Exception:
         return None
