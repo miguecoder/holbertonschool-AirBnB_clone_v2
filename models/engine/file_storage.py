e7d42dd182421fb64cb9d4b56d8eecc335837770
miguecoder <mmarqzm@gmail.com>
2022-09-14 21:03:27 -0500

    Improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,10 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del (self.__objects[key])
+
+    def close(self):
+        """
+        Instance public method that calls reload method
+        for deserializing the JSON file to objects
+        """
+        self.reload()
--- models/user.py
+++ models/user.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user', cascade='delete')
-    reviews = relationship('Review', backref='user', cascade='delete')
+class User(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
+    """
+    This class defines a user by various attributes
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+            "Place",
+            cascade="all,delete",
+            backref=backref("user", cascade="all,delete"),
+            passive_deletes=True,
+            single_parent=True
+        )
+        reviews = relationship(
+            "Review",
+            cascade="all,delete",
+            backref=backref("user", cascade="all,delete"),
+            passive_deletes=True,
+            single_parent=True
+        )
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
